80b67b9bc08e8901344630e89f4b956cf3eb9b2c
shashank460 <shashankchurk2022@gmail.com>
2026-08-24 21:41:58 +0530

    expose EKS node security group for database access

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -10,20 +10,17 @@ variable "cluster_name" { type = string }
 variable "environment" { type = string }
 variable "private_subnet_ids" { type = list(string) }
 
- data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "cluster_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
+    principals { type = "Service", identifiers = ["eks.amazonaws.com"] }
   }
 }
 
 resource "aws_iam_role" "cluster" {
-  name               = "${var.cluster_name}-cluster-role"
+  name = "${var.cluster_name}-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.cluster_assume.json
 }
 
@@ -35,47 +32,37 @@ resource "aws_iam_role_policy_attachment" "cluster" {
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
-
   vpc_config {
     subnet_ids              = var.private_subnet_ids
     endpoint_private_access = true
     endpoint_public_access  = true
   }
-
-  access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
-  }
-
+  access_config { authentication_mode = "API_AND_CONFIG_MAP" }
   depends_on = [aws_iam_role_policy_attachment.cluster]
 }
 
 data "aws_iam_policy_document" "node_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
+    principals { type = "Service", identifiers = ["ec2.amazonaws.com"] }
   }
 }
 
 resource "aws_iam_role" "node" {
-  name               = "${var.cluster_name}-node-role"
+  name = "${var.cluster_name}-node-role"
   assume_role_policy = data.aws_iam_policy_document.node_assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "node_worker" {
-  role       = aws_iam_role.node.name
+  role = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
-
 resource "aws_iam_role_policy_attachment" "node_cni" {
-  role       = aws_iam_role.node.name
+  role = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
-
 resource "aws_iam_role_policy_attachment" "node_ecr" {
-  role       = aws_iam_role.node.name
+  role = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
 }
 
@@ -84,25 +71,11 @@ resource "aws_eks_node_group" "this" {
   node_group_name = "${var.cluster_name}-nodes"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.private_subnet_ids
-
-  instance_types = ["t3.small"]
-  capacity_type  = "ON_DEMAND"
-
-  scaling_config {
-    desired_size = 2
-    min_size     = 2
-    max_size     = 4
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.node_worker,
-    aws_iam_role_policy_attachment.node_cni,
-    aws_iam_role_policy_attachment.node_ecr
-  ]
+  instance_types  = ["t3.small"]
+  capacity_type   = "ON_DEMAND"
+  scaling_config { desired_size = 2, min_size = 2, max_size = 4 }
+  update_config { max_unavailable = 1 }
+  depends_on = [aws_iam_role_policy_attachment.node_worker, aws_iam_role_policy_attachment.node_cni, aws_iam_role_policy_attachment.node_ecr]
 }
 
 resource "aws_eks_access_entry" "deployer" {
@@ -115,11 +88,31 @@ resource "aws_eks_access_policy_association" "deployer" {
   cluster_name  = aws_eks_cluster.this.name
   principal_arn = data.aws_caller_identity.current.arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  access_scope { type = "cluster" }
+}
 
-  access_scope {
-    type = "cluster"
+resource "aws_security_group" "nodes" {
+  name        = "${var.cluster_name}-nodes"
+  description = "EKS worker node traffic"
+  vpc_id      = data.aws_subnet.selected.vpc_id
+
+  ingress {
+    description = "Node to node"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+data "aws_subnet" "selected" { id = var.private_subnet_ids[0] }
+
 output "cluster_name" { value = aws_eks_cluster.this.name }
 output "node_role_arn" { value = aws_iam_role.node.arn }
+output "node_security_group_id" { value = aws_security_group.nodes.id }
