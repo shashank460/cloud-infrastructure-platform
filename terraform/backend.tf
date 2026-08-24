ee0359ce78264dd9a678f418d6ca45aea3b22f3b
shashank460 <shashankchurk2022@gmail.com>
2026-08-24 21:46:23 +0530

    remove misplaced root backend configuration

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "shashank-cloud-platform-tfstate"
-    key            = "cloud-platform/dev/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "shashank-cloud-platform-tf-locks"
-  }
-}
