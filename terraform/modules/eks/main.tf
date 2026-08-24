50b2dad958ee9e6781e1128f80e9b94db0ad7cac
shashank460 <shashankchurk2022@gmail.com>
2026-08-24 21:44:32 +0530

    harden EKS API access and node security group

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -9,6 +9,10 @@ terraform {
 variable "cluster_name" { type = string }
 variable "environment" { type = string }
 variable "private_subnet_ids" { type = list(string) }
+variable "public_access_cidrs" {
+  type    = list(string)
+  default = []
+}
 
 data "aws_caller_identity" "current" {}
 
@@ -20,22 +24,60 @@ data "aws_iam_policy_document" "cluster_assume" {
 }
 
 resource "aws_iam_role" "cluster" {
-  name = "${var.cluster_name}-cluster-role"
+  name               = "${var.cluster_name}-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.cluster_assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "cluster" {
-  role = aws_iam_role.cluster.name
+  role       = aws_iam_role.cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+resource "aws_security_group" "nodes" {
+  name        = "${var.cluster_name}-nodes"
+  description = "Security group for EKS worker nodes"
+  vpc_id      = data.aws_vpc.cluster.id
+
+  ingress {
+    description = "Node-to-node traffic"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    self        = true
+  }
+
+  ingress {
+    description     = "Application traffic from the EKS cluster security group"
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_eks_cluster.this.vpc_config[0].cluster_security_group_id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "cluster" {
+  id = data.aws_subnet.private.vpc_id
+}
+
+data "aws_subnet" "private" {
+  id = var.private_subnet_ids[0]
+}
+
 resource "aws_eks_cluster" "this" {
-  name = var.cluster_name
+  name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
   vpc_config {
-    subnet_ids = var.private_subnet_ids
+    subnet_ids              = var.private_subnet_ids
     endpoint_private_access = true
-    endpoint_public_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = var.public_access_cidrs
   }
   access_config { authentication_mode = "API_AND_CONFIG_MAP" }
   depends_on = [aws_iam_role_policy_attachment.cluster]
@@ -49,48 +91,65 @@ data "aws_iam_policy_document" "node_assume" {
 }
 
 resource "aws_iam_role" "node" {
-  name = "${var.cluster_name}-node-role"
+  name               = "${var.cluster_name}-node-role"
   assume_role_policy = data.aws_iam_policy_document.node_assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "node_worker" {
-  role = aws_iam_role.node.name
+  role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 resource "aws_iam_role_policy_attachment" "node_cni" {
-  role = aws_iam_role.node.name
+  role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 resource "aws_iam_role_policy_attachment" "node_ecr" {
-  role = aws_iam_role.node.name
+  role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
 }
 
 resource "aws_eks_node_group" "this" {
-  cluster_name = aws_eks_cluster.this.name
+  cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-nodes"
-  node_role_arn = aws_iam_role.node.arn
-  subnet_ids = var.private_subnet_ids
-  instance_types = ["t3.small"]
-  capacity_type = "ON_DEMAND"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.private_subnet_ids
+  instance_types  = ["t3.small"]
+  capacity_type   = "ON_DEMAND"
+  remote_access { ec2_ssh_key = null }
   scaling_config { desired_size = 2, min_size = 2, max_size = 4 }
   update_config { max_unavailable = 1 }
+  launch_template {
+    id      = aws_launch_template.nodes.id
+    version = aws_launch_template.nodes.latest_version
+  }
   depends_on = [aws_iam_role_policy_attachment.node_worker, aws_iam_role_policy_attachment.node_cni, aws_iam_role_policy_attachment.node_ecr]
 }
 
+resource "aws_launch_template" "nodes" {
+  name_prefix   = "${var.cluster_name}-nodes-"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [aws_security_group.nodes.id]
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+  }
+}
+
 resource "aws_eks_access_entry" "deployer" {
-  cluster_name = aws_eks_cluster.this.name
+  cluster_name  = aws_eks_cluster.this.name
   principal_arn = data.aws_caller_identity.current.arn
-  type = "STANDARD"
+  type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "deployer" {
-  cluster_name = aws_eks_cluster.this.name
+  cluster_name  = aws_eks_cluster.this.name
   principal_arn = data.aws_caller_identity.current.arn
-  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   access_scope { type = "cluster" }
 }
 
 output "cluster_name" { value = aws_eks_cluster.this.name }
 output "node_role_arn" { value = aws_iam_role.node.arn }
 output "cluster_security_group_id" { value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id }
+output "node_security_group_id" { value = aws_security_group.nodes.id }
