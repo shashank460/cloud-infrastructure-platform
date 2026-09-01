04e11728b5331b9466ff76b99982608640472b86
shashank460 <shashankchurk2022@gmail.com>
2026-09-01 11:30:16 +0530

    add production outputs

---
+++ terraform/environments/prod/outputs.tf
@@ -0,0 +1,4 @@
+output "eks_cluster_name" { value = module.eks.cluster_name }
+output "rds_endpoint" { value = module.rds.endpoint }
+output "s3_bucket" { value = module.s3.bucket_name }
+output "monitoring_dashboard" { value = module.monitoring.dashboard_name }
